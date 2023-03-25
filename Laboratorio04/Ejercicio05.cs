2a6e9e7de351592afb3f625e57c03a086b242947
Kenneth <elkennethcw@gmail.com>
2023-03-24 20:38:34 -0600

    ejercicio number 5 hecho completo

--- Laboratorio04/Ejercicio05.cs
+++ Laboratorio04/Ejercicio05.cs
@@ -10,7 +10,20 @@ namespace Laboratorio04
     {
         public int[] CalcularBilletes(int cantidad, int[,] billetes)
         {
-            return new int[] {};
+            int[] resultado = new int[billetes.GetLength(0)];
+            for (int i = 0; i < billetes.GetLength(0); i++)
+            {
+                int denominacion = billetes[i, 0];
+                int limite = billetes[i, 1];
+                int billetesNecesarios = cantidad / denominacion;
+                if (limite >= 0 && billetesNecesarios > limite)
+                {
+                    billetesNecesarios = limite;
+                }
+                resultado[i] = billetesNecesarios;
+                cantidad -= denominacion * billetesNecesarios;
+            }
+            return resultado;
         }
     }
 }
